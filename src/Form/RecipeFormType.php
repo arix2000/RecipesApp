dba07e7a8dd8cd3ff333bd312cdc7e07ebe98076
arix2000 <arix20003@gmail.com>
2024-06-30 13:12:55 +0200

    add better ui validation for form

--- src/Form/RecipeFormType.php
+++ src/Form/RecipeFormType.php
@@ -10,6 +10,7 @@ use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\Validator\Constraints\Url;
 
 class RecipeFormType extends AbstractType
@@ -19,15 +20,25 @@ class RecipeFormType extends AbstractType
         $builder
             ->add('title', TextType::class, [
                 'label' => 'Title',
-                'attr' => ['class' => 'form-input-dark w-full mb-4 bg-gray-800 text-gray-100 border border-gray-600 rounded-md px-3 py-2 leading-tight focus:outline-none focus:shadow-outline']
+                'required' => false,
+                'constraints' => [
+                    new NotBlank(message: 'Title is required'),
+                ],
+                'attr' => ['class' => 'form-input-dark w-full bg-gray-800 text-gray-100 border border-gray-600 rounded-md px-3 py-2 leading-tight focus:outline-none focus:shadow-outline']
             ])
             ->add('ingredients', TextareaType::class, [
-                'label' => 'Ingredients',
-                'attr' => ['class' => 'form-textarea-dark w-full mb-4 bg-gray-800 text-gray-100 border border-gray-600 rounded-md px-3 py-2 leading-tight focus:outline-none focus:shadow-outline', 'rows' => 5]
+                'required' => false,
+                'constraints' => [
+                    new NotBlank(message: 'Ingredients is required'),
+                ],
+                'attr' => ['class' => 'form-textarea-dark w-full bg-gray-800 text-gray-100 border border-gray-600 rounded-md px-3 py-2 leading-tight focus:outline-none focus:shadow-outline', 'rows' => 5]
             ])
             ->add('directions', TextareaType::class, [
-                'label' => 'Directions',
-                'attr' => ['class' => 'form-textarea-dark w-full mb-4 bg-gray-800 text-gray-100 border border-gray-600 rounded-md px-3 py-2 leading-tight focus:outline-none focus:shadow-outline', 'rows' => 8]
+                'required' => false,
+                'constraints' => [
+                    new NotBlank(message: 'Directions is required'),
+                ],
+                'attr' => ['class' => 'form-textarea-dark w-full bg-gray-800 text-gray-100 border border-gray-600 rounded-md px-3 py-2 leading-tight focus:outline-none focus:shadow-outline', 'rows' => 8]
             ])
             ->add('source', ChoiceType::class, [
                 'label' => 'Source:',
@@ -39,10 +50,9 @@ class RecipeFormType extends AbstractType
                 'expanded' => false,
                 'multiple' => false,
                 'attr' => ['class' => 'form-radio-dark mb-4 ml-4 bg-gray-800 text-gray-100 border border-gray-600 rounded-md px-3 py-2 leading-tight focus:outline-none focus:shadow-outline'],
-                'data' => 'From website', // Default value
+                'data' => 'From website',
             ])
             ->add('link', TextType::class, [
-                'label' => 'Link',
                 'constraints' => [
                     new Url(['message' => 'This is not a valid URL']),
                 ],
@@ -50,7 +60,6 @@ class RecipeFormType extends AbstractType
                 'attr' => ['class' => 'tooltip-trigger form-input-dark w-full bg-gray-800 text-gray-100 border border-gray-600 rounded-md px-3 py-2 leading-tight focus:outline-none focus:shadow-outline', 'style' => 'display:none;'],
             ])
             ->add('site', TextType::class, [
-                'label' => 'Site',
                 'required' => false,
                 'constraints' => [
                     new Url(['message' => 'This is not a valid URL']),
@@ -59,9 +68,11 @@ class RecipeFormType extends AbstractType
 
             ])
             ->add('ner', TextType::class, [
-                'label' => 'Ner',
-                'required' => true,
-                'attr' => ['class' => 'form-textarea-dark w-full mb-4 bg-gray-800 text-gray-100 border border-gray-600 rounded-md px-3 py-2 leading-tight focus:outline-none focus:shadow-outline', 'rows' => 8]
+                'required' => false,
+                'constraints' => [
+                    new NotBlank(message: 'Ingredients (no quantities) is required'),
+                ],
+                'attr' => ['class' => 'form-textarea-dark w-full bg-gray-800 text-gray-100 border border-gray-600 rounded-md px-3 py-2 leading-tight focus:outline-none focus:shadow-outline', 'rows' => 8]
             ])
             ->add('imageUrl', FileType::class, [
                 'label' => 'Add image',
