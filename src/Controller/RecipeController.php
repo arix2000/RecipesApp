56ac1228e713e50e51809f072ff67a808cb48931
arix2000 <arix20003@gmail.com>
2024-07-01 20:34:49 +0200

    refactor RecipeController.php

--- src/Controller/RecipeController.php
+++ src/Controller/RecipeController.php
@@ -6,6 +6,7 @@ use App\Entity\Recipe;
 use App\Entity\User;
 use App\Form\RecipeFormType;
 use App\Repository\RecipeRepository;
+use App\Services\RecipeService;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\FormInterface;
@@ -19,17 +20,20 @@ class RecipeController extends AbstractController
 {
     private RecipeRepository $recipeRepository;
     private EntityManagerInterface $entityManager;
-    private RequestStack $requestStack;
+    private RecipeService $recipeService;
 
     public function __construct(
         RecipeRepository       $recipeRepository,
         EntityManagerInterface $entityManager,
-        RequestStack           $requestStack
+        RequestStack           $requestStack,
+        string                 $projectDir
     )
     {
         $this->recipeRepository = $recipeRepository;
         $this->entityManager = $entityManager;
-        $this->requestStack = $requestStack;
+        $request = $requestStack->getCurrentRequest();
+        $hostUrl = $request->getSchemeAndHttpHost();
+        $this->recipeService = new RecipeService($projectDir, $hostUrl);
     }
 
     #[Route('/', name: 'recipes')]
@@ -55,7 +59,7 @@ class RecipeController extends AbstractController
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
             $newRecipe = $form->getData();
-            $newRecipe = $this->getUpdatedRecipe($newRecipe, $form, $user);
+            $newRecipe = $this->recipeService->getUpdatedRecipe($newRecipe, $form, $user);
             $this->entityManager->persist($newRecipe);
             $this->entityManager->flush();
             return $this->redirectToRoute('recipes');
@@ -77,55 +81,13 @@ class RecipeController extends AbstractController
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
             $recipe = $form->getData();
-            $this->getUpdatedRecipe($recipe, $form, $user);
+            $this->recipeService->getUpdatedRecipe($recipe, $form, $user);
             $this->entityManager->flush();
             return $this->redirectToRoute('recipe', ["id" => $id]);
         }
 
 
         return $this->render("recipe/edit.html.twig", ["form" => $form->createView()]);
-    }
-
-    function getUpdatedRecipe(Recipe $recipe, FormInterface $form, User $user): Recipe|Response
-    {
-        $recipe->setUser($user);
-        $recipe->setIngredients($this->stringToJsonArray($recipe->getIngredients()));
-        $recipe->setDirections($this->stringToJsonArray($recipe->getDirections()));
-        $recipe->setNer($this->stringToJsonArray($recipe->getNer()));
-
-        $imagePath = $form->get('imageUrl')->getData();
-        if ($imagePath) {
-            $newFileName = uniqid() . '.' . $imagePath->guessExtension();
-            try {
-                $imagePath->move(
-                    $this->getParameter('kernel.project_dir') . '/public/recipe/uploads',
-                    $newFileName
-                );
-            } catch (FileException $e) {
-                return new Response($e->getMessage());
-            }
-            $request = $this->requestStack->getCurrentRequest();
-            $hostUrl = $request->getSchemeAndHttpHost();
-            $recipe->setImageUrl($hostUrl . "/recipe/uploads/" . $newFileName);
-        }
-        return $recipe;
-    }
-
-
-    function stringToJsonArray($inputString): string
-    {
-        $normalizedString = str_replace("\r\n", "\n", $inputString);
-        $normalizedString = str_replace("\r", "\n", $normalizedString);
-
-        $linesArray = explode("\n", $normalizedString);
-        $filteredArray = array_filter($linesArray, function ($line) {
-            return trim($line) !== '';
-        });
-        if (empty($filteredArray)) {
-            $filteredArray = [$inputString];
-        }
-        $filteredArray = array_values($filteredArray);
-        return json_encode($filteredArray);
     }
 
     #[Route('/recipe/delete/{id}', name: 'delete_recipe')]
---
+++ src/Services/RecipeService.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Services;
+
+use App\Entity\Recipe;
+use App\Entity\User;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\Response;
+
+class RecipeService
+{
+    private string $projectDir;
+    private string $hostUrl;
+
+    public function __construct(string $projectDir, string $hostUrl)
+    {
+        $this->projectDir = $projectDir;
+        $this->hostUrl = $hostUrl;
+    }
+
+    function getUpdatedRecipe(Recipe $recipe, FormInterface $form, User $user): Recipe|Response
+    {
+        $recipe->setUser($user);
+        $recipe->setIngredients($this->stringToJsonArray($recipe->getIngredients()));
+        $recipe->setDirections($this->stringToJsonArray($recipe->getDirections()));
+        $recipe->setNer($this->stringToJsonArray($recipe->getNer()));
+
+        $imagePath = $form->get('imageUrl')->getData();
+        if ($imagePath) {
+            $newFileName = uniqid() . '.' . $imagePath->guessExtension();
+            try {
+                $imagePath->move(
+                    $this->projectDir . '/public/recipe/uploads',
+                    $newFileName
+                );
+            } catch (FileException $e) {
+                return new Response($e->getMessage());
+            }
+            $recipe->setImageUrl($this->hostUrl . "/recipe/uploads/" . $newFileName);
+        }
+        return $recipe;
+    }
+
+
+    function stringToJsonArray($inputString): string
+    {
+        $normalizedString = str_replace("\r\n", "\n", $inputString);
+        $normalizedString = str_replace("\r", "\n", $normalizedString);
+
+        $linesArray = explode("\n", $normalizedString);
+        $filteredArray = array_filter($linesArray, function ($line) {
+            return trim($line) !== '';
+        });
+        if (empty($filteredArray)) {
+            $filteredArray = [$inputString];
+        }
+        $filteredArray = array_values($filteredArray);
+        return json_encode($filteredArray);
+    }
+}
